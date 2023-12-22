c5263aa3643f00a99e728abc8f7610085c6dd988
samariddin1775 <freefire177515@gmail.com>
2023-12-22 18:02:10 +0500

    proxy added

--- ShopBot/app.py
+++ ShopBot/app.py
@@ -1,17 +1,27 @@
 from aiogram import Dispatcher, executor, Bot, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from default_keyboards import KeyboardButton, ReplyKeyboardMarkup, shop_main_menu
+from environs import Env
 
 
-token = "6434315395:AAFRvc7Kznok_iG2RyGEnBxJ7CnXUn034Hw"
-bot = Bot(token=token)
-dp = Dispatcher(bot)
+env = Env()
+env.read_env()
+
+
+BOT_TOKEN = env.str("TOKEN")
+
+storage = MemoryStorage()
+bot = Bot(token=BOT_TOKEN, proxy="https://proxy.server:3128" )
+dp = Dispatcher(bot, storage=storage)
 
 
 @dp.message_handler(commands="start")
 async def start_handler(message: types.Message):
     text = "Assalomu alaykum, hurmatli mehmon"
     await message.answer(text=text, reply_markup=shop_main_menu)
+
+
 
 
 
